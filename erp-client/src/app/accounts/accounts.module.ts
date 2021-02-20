a74e95d55bbb94f23700e48ac8d280f05fd62a25
suresh <sureshsep03@gmail.com>
2021-02-20 11:36:42 +0530

    Accounts module creation Initial

---
+++ erp-client/src/app/accounts/accounts.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ManageGroupComponent } from './manage-group/manage-group.component';
+
+
+
+@NgModule({
+  declarations: [ManageGroupComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AccountsModule { }
