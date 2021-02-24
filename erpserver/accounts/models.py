fbbc70bbb9c0c9f38cad24d0b3b084cfbad4de1e
suresh <sureshsep03@gmail.com>
2021-02-24 19:04:25 +0530

    account

--- erpserver/accounts/models.py
+++ erpserver/accounts/models.py
@@ -12,3 +12,22 @@ class Groups(AuditUuidModelMixin):
         return  self.group_name
 
 
+class SubGroups(AuditUuidModelMixin):
+    sub_group_name = models.CharField(max_length=1000)
+    category = models.ForeignKey(Groups,on_delete=models.CASCADE())
+
+    def __str__(self):
+        return  self.group_name
+
+
+class TdsSection(AuditUuidModelMixin):
+    # ref_no = models.CharField(max_length=255,null=True)
+    section_number = models.CharField(max_length=255)
+    source = models.CharField(max_length=255)
+    payee_type = models.CharField(max_length=255)
+    thershold_limit = models.DecimalField(max_digits=10,decimal_places=2)
+    tds_rate = models.DecimalField(max_digits=10,decimal_places=2)
+
+    def __str__(self):
+        return self.section_number
+
