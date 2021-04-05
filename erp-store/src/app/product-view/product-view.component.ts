e2e04f33e5626500cf3458eed19e248b6d164ab0
suresh <sureshsep03@gmail.com>
2021-04-05 13:23:06 +0530

    Rating and review fix

--- erp-store/src/app/product-view/product-view.component.ts
+++ erp-store/src/app/product-view/product-view.component.ts
@@ -104,7 +104,10 @@ export class ProductViewComponent implements OnInit {
       //
       // console.log(data);
       if(data.length > 0){
-        this.ratings = data;
+        data.forEach(element => {
+          this.ratings.push(element);  
+        });
+        
       }
     });
   }
