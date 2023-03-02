9191a215750ba6ecad05f5f0deff7bfb2cd77f58
root <root@venu@sanaditechnologies.com>
2023-03-02 17:34:23 +0530

    added entrypoint.sh

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -e
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+uwsgi --socket :8000 --master  --enable-threads backend.wsgi
